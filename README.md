0e9432834e55e804ba93b3c35a751a68e9747484
jkinsfat <jkinsfat@w.edu>
2016-04-13 14:51:15 -0700

    Graphs Up and Running

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Antibiotics-Triviz
+
+QUESTION 1: Which bacteria have the highest resistance to Penicilin?
+
+QUESTION 2: Is Gram-positive Bacteria more susceptible to Streptomycin or Neomycin?
+
+QUESTION 3: Which antibiotic is most consistent?
--- js/triviz.js
+++ js/triviz.js
@@ -1,25 +1,191 @@
 $(function() {
-	d3.csv("data/antibiotics_data.csv", function(error, data){
+	//loads data file
+    d3.csv("data/antibiotics_data.csv", function(error, data) {
         function unpack(rows, key) {
       		return rows.map(function(row) { return row[key]; });
         }
 
-        var bacteria = unpack(data, 'Bacteria')
-        	penicilin = unpack(data, 'Penicilin')
-        	streptomycin = unpack(data, 'Streptomycin')
-        	neomycin = unpack(data, 'Neomycin')
-        	gramStain = unpack(data, 'Gram Staining')
+        //retrieve rows from file
+        var bacteria = unpack(data, 'Bacteria '),
+        	penicilin = unpack(data, 'Penicilin'),
+        	streptomycin = unpack(data, 'Streptomycin '),
+        	neomycin = unpack(data, 'Neomycin'),
+        	gramStain = unpack(data, 'Gram Staining '),
+            //Set up indices for Gram positive/negative rows
+            negInd = [],
+            posInd = [],
+            //Set up label and color arrays for Gram sorted rows
+            nameArray = [],
+            gramLabels = [],
+            directLabels = [],
+            colorArray = [];
+        
+        //Finds indices of Gram-Positive and Gram-Negative rows
+        //and creates list of colors and labels in the positive/negative sequence.
+        for (var i = 0; i < gramStain.length; i++){
+            if (gramStain[i] == 'negative'){
+                negInd.push(i);
+                nameArray.push('Gram Negative');
+                colorArray.push('rgb(255, 128, 223)');
+                gramLabels.push('Neg');
+            } else {
+                posInd.push(i);
+                nameArray.push('Gram Positive');
+                colorArray.push('rgb(153, 0, 204)');
+                gramLabels.push('Pos');
+            }
+        }
 
-        var data = [{
-        	values: penicilin,
-        	labels: bacteria,
-            type: 'pie'
+        //Selects all values in given array at indexes in the index list.
+        function valueSelect(index, array) {
+            var values = []
+            for (var i = 0; i < index.length; i++){
+                values.push(array[index[i]]);
+            }
+            return values;
+        }
+
+        //Make arrays of the row values in Gram-Negative and Gram-Positive rows
+        var bactPos = valueSelect(posInd, bacteria),
+            bactNeg = valueSelect(negInd, bacteria),
+            penPos = valueSelect(posInd, penicilin),
+            penNeg = valueSelect(negInd, penicilin),
+            strepPos = valueSelect(posInd, streptomycin),
+            strepNeg = valueSelect(negInd, streptomycin),
+            neoPos = valueSelect(posInd, neomycin),
+            neoNeg = valueSelect(negInd, neomycin);
+
+        //Data For First Visualizations
+        //Bar Chart Data: y = labels, x = quantitative data
+        var data1 = [{
+            name: nameArray,
+            x: bacteria,
+            y: penicilin,
+            type: 'bar',
+            marker: {
+                color: colorArray,
+
+            }
         }]
 
-        var layout = {
-            height: 400,
-            width: 500
+        //Creates list of 'pos' and 'neg' labels in same sequence as Gram test data.
+        for (var i = 0; i < gramLabels.length; i++) {    
+            var label = {
+                x: bacteria[i],
+                y: penicilin[i],
+                text: gramLabels[i],
+                xanchor: 'center',
+                yanchor: 'bottom',
+                showarrow: false
+            }
+               directLabels.push(label);
+        };
+
+        //Layout of visualization one
+        var layout1 = {
+            title: 'Minimum Inhibitory Concentration of Penicilan for Gram Postive/Negative Bacteria',
+            annotations: directLabels,
+            height: 550,
+            margin: {
+                b: 120
+            },
+            yaxis: {
+                title: 'Penicilin (Units)'
+            },
+            paper_bgcolor: 'rgb(200,200,200)',
+            plot_bgcolor: 'rgb(200,200,200)'
+        };
+        //Creates visualization one
+        Plotly.newPlot('viz1', data1, layout1, {staticPlot: true});
+        
+        //Data for Second Visualization
+        //Data for scatterplot sorted by Gram-negativ/positive rows
+        var data2 = [{
+            name: "Gram Positive",
+            x: strepPos,
+            y: neoPos,
+            type: 'scatter',
+            mode: 'markers',
+            marker: {
+                color: 'rgb(153, 0, 204)',
+                size: 8
+            }
+        },{
+            name: "Gram Negative",
+            x: strepNeg,
+            y: neoNeg,
+            type: 'scatter',
+            mode: 'markers',
+            marker: {
+                color: 'rgb(255, 128, 223)',
+                size: 8
+            }
+        }];
+
+        //Layout for visualization two
+        var layout2 = {
+            title: 'Resistance of Gram-Positive and Gram-Negative Bacteria to Antibiotics',
+            xaxis: {
+                title: 'MIC of Streptomycin (Units)'
+            },
+            yaxis: {
+                title: 'MIC of Neomycin (Units)'
+            }
         }
-    	Plotly.plot('viz1', data);
+        //Creates Visualization 2
+        Plotly.newPlot('viz2', data2, layout2, {staticPlot: true});
+        
+        //Data for Third Visualization:
+        //Calculates average of the numbers in a list       
+        function average(data, length){
+            var sum = data.reduce(function(sum, value){
+                return sum + +value;
+            }, 0);
+
+            var mean = sum / length;
+            return mean;
+        }
+
+        //Calculates the Coefficient of Variation for a list of data
+        function getStats(data){
+            //Get mean
+            var mean = average(data, data.length);
+
+            //Use mean to find square difference of each value: (x - mean) ^ 2
+            var sqrDifferences = data.map(function(value){
+                var difference = +value - mean;
+                var square = difference * difference;
+                return square;
+            });
+
+            //Calculate variance: s^2
+            var aveSquareDiff = average(sqrDifferences, data.length - 1);
+            //Calculate standard deviation: s
+            var stdDev = Math.sqrt(aveSquareDiff);
+            //Calculate coefficient of variation: s / mean
+            coefOfVar = stdDev / mean;
+            return coefOfVar; 
+        }
+
+        //Data for bar chart where x = antibiotic and y = the coefficient of variation
+        var data3 = [{
+            x: ['Penicilin', 'Streptomycin', 'Neomycin'],
+            y: [getStats(penicilin), getStats(streptomycin), getStats(neomycin)],
+            type: 'bar'
+        }]
+
+        //Layout for visualization 3
+        var layout3 = {
+            title: 'Consistency of Minimum Inhibitory Concentration of Antibiotics for Bacteria',
+            yaxis: {
+                title: 'Coefficient of Variation'
+            },
+            xaxis: {
+                title: 'Antibiotic'
+            }
+        }
+
+        //Creates Visualization three
+        Plotly.newPlot('viz3', data3, layout3), {staticPlot: true};
     });
 });
